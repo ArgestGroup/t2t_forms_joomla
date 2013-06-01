35f2db415a1007766418bace2dd4b698d96ef94b
undead <undead@silverdark.(none)>
2013-06-01 12:41:33 +0300

    release

--- t2t/T2TRouter.php
+++ t2t/T2TRouter.php
@@ -1,14 +1,9 @@
 <?php
-	define( '_JEXEC', 1 );
-	define( 'JPATH_BASE', realpath($_SERVER['DOCUMENT_ROOT']));
-	define( 'DS', DIRECTORY_SEPARATOR );
-	 
-	require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
-	require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
-	$mainframe = JFactory::getApplication('site');
-	$mainframe->initialise();
 
-	require_once dirname(__FILE__).'/T2TForms.php';
+	// Инициализация компонента
+	include('T2TForms.php');
+	// Инициализируем статику
+	T2TForms::app();  
 	// Обработка логаут из режима авторизации форм
 	T2TForms::logout();
 	// Обработка ajax запросов
